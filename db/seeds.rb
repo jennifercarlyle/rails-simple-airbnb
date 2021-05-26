de6d9c4d87f79e4c03f2c936c49844b5a88ac1e8
Jennifer Carlyle <jennifercarlyle@gmail.com>
2021-05-26 16:21:00 +0100

    add an index view page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'cleaning database...'
+Flat.destroy_all
+
+puts 'creating flats...'
+10.times do
+  flat = Flat.create!(
+    name: Faker::Address.street_name,
+    address: Faker::Address.street_address,
+    description: Faker::GreekPhilosophers.quote,
+    price_per_night: rand(50..400),
+    number_of_guests: rand(1..10)
+  )
+  puts "created #{flat.name}"
+end
+puts 'finished!'
